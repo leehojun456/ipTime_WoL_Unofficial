75b3ff69784973ca981cf4f120a4ebd6a4de5eb7
CALB <hppmm@naver.com>
2025-12-29 13:06:16 +0900

    Fix Login Session

--- app/src/main/java/dev/calb456/iptimewol/network/RouterApiService.kt
+++ app/src/main/java/dev/calb456/iptimewol/network/RouterApiService.kt
@@ -5,6 +5,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.android.Android
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.cookies.HttpCookies
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.HttpResponse
@@ -25,6 +26,7 @@ class RouterApiService {
                 coerceInputValues = true // Useful for cases where API returns null for non-nullable fields with defaults
             })
         }
+        install(HttpCookies)
     }
 
     private suspend inline fun <reified Req, reified Res> makePostRequest(
